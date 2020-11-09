acd15b32ff816e9e14797e0731bf6a0dd13ff3e8
bprpforever <ioleynik@zuzex.com>
2020-11-09 22:26:25 +0300

    fixed test_items file

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_basket_btn(browser):
+def test_check_basket_button_exists(browser):
     browser.get(link)
     basket_btn = browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
     if basket_btn:
