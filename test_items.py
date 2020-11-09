0bb0f864a391cd8b100b640626ac43b158650eee
bprpforever <ioleynik@zuzex.com>
2020-11-09 22:48:37 +0300

    fixed test_items to make it better

--- test_items.py
+++ test_items.py
@@ -1,14 +1,17 @@
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
 def test_check_basket_button_exists(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    basket_btn = browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
-    if basket_btn:
-        assert True
-    else:
-        assert False, \
-            f"Test {basket_btn} failed! No 'add to basket' button on the page"
+    basket_btn_xpath = "//button[@class='btn btn-lg btn-primary btn-add-to-basket']"
 
+    def is_element_present(browser):
+        try:
+            browser.implicitly_wait(5)
+            browser.find_element_by_xpath(basket_btn_xpath)
+            return True
+        except:
+            return False
+
+    assert is_element_present(browser) == True, \
+        f"Tes failed! No {basket_btn_xpath} button on the page"
